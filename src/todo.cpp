5d328b829f9be4e59b816f20fd9f4e9bd12bc775
Don Castillo <don.castillo@uleth.ca>
2020-10-28 02:14:16 -0600

    added more files

---
+++ src/todo.cpp
@@ -0,0 +1,47 @@
+/**
+
+
+Meld = set of 3 or 4 cards with the same rank
+      = set of 3 or 4 cards with the same suit (should be in sequence)
+
+
+
+
+Rules of Rummy Game
+
+[] Rummy is played by min of 2 and max of 6 players
+
+
+DEAL
+[]  if 2 players, each one gets 10 cards, rest is in the deck
+[]  if 3 - 4 players, each one gets 7 cards, rest is in the deck
+[]  if 5 - 6 players, each one gets 6 cards, rest is in the deck
+
+[] after the deal, the top card in the deck is placed beside the deck with its face facing up
+
+
+GAME PLAY
+[] player has an option to draw the top card of the deck OR draw the top card of the discard pile
+[] player has an option to lay down possible meld
+[] each player's turns ends by discarding a card from his hand and putting it on top of the discard pile
+[] if the player has drawn from the discard pile, he may not discad the same card on that turns
+[] each player can put down a card the belongs to a meld the is already put down
+
+
+WHO WINS?
+[] Player who gets to empty his hands winsQ
+[] If all of their remaining cards are mathced, the player may lay them down without discarding on their last turn. This ends the game and there is no further play.
+[] If the last card of the stock has been drawn and no player has gone out, the next player in turn may either take the top of the discard pile, or may turn the discard
+  pile over to form a new stock (front card becomes the back card, vice-versa) and draw the top card. Play then proceeds as before.
+
+SCORING
+[] player who finishes off the round, receives points as determined by the other player's card's face value at hand
+[]  if the player lays off all his cards in hands in one round, he goes rummy. he receives double the face values on the cards from the hand of the other player.
+[] total number of points is agreed upon before the game.
+
+
+
+
+
+
+*/

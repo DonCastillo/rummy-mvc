c1469e768232dd59002bc71c9d8088448ce0baff
Don Castillo <don.castillo@uleth.ca>
2020-10-31 23:16:06 -0600

    fixed

--- test/RummyTest.cpp
+++ test/RummyTest.cpp
@@ -204,7 +204,7 @@ TEST(RummyTest, draw_card_from_deck_or_discard_pile) {
   Deck* d = new Deck();
   MockUI ui;
 
-  Game* game = new Rummy(&ui, d);
+  Rummy* game = new Rummy(&ui, d);
   Player* michael = new Player("Michael");
   Player* dwight = new Player("Dwight");
   game->addPlayer(michael);

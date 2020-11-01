75e8b0563a125f3ceeb8ce577d4bad7557f353ca
Don Castillo <don.castillo@uleth.ca>
2020-10-31 18:29:34 -0600

    tested deal cards with 3 players

--- test/RummyTest.cpp
+++ test/RummyTest.cpp
@@ -10,6 +10,7 @@ using ::testing::Return;
 using ::testing::_;
 using ::testing::Expectation;
 
+// deal with 2 players
 TEST(RummyTest, deal_cards_with_2_players) {
   MockDeck d;
   MockUI ui;
@@ -36,6 +37,36 @@ TEST(RummyTest, deal_cards_with_2_players) {
     EXPECT_EQ(10, p->getHand()->size());
 
   delete game;
+  delete michael;
+  delete dwight;
+}
+
+// deal with 3 players
+TEST(RummyTest, deal_cards_with_3_players) {
+  MockDeck d;
+  MockUI ui;
+
+  EXPECT_CALL(d, getCard())
+  .Times(21);
+
+  Game* game = new Rummy(&ui, &d);
+  Player* michael = new Player("Michael");
+  Player* dwight = new Player("Dwight");
+  Player* jim = new Player("Jim");
+
+  game->addPlayer(michael);
+  game->addPlayer(dwight);
+  game->addPlayer(jim);
+
+  EXPECT_EQ(game->getPlayers().size(), 3);
+
+  game->dealCards(game->getPlayers());
+
+  for (Player* p : game->getPlayers())
+    EXPECT_EQ(7, p->getHand()->size());
+
+  delete game;
+  delete jim;
   delete michael;
   delete dwight;
 }

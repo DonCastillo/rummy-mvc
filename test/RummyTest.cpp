5e366749d34974abc2435c87e120ed56939acc30
Don Castillo <don.castillo@uleth.ca>
2020-10-31 18:38:27 -0600

    added more test units for dealCards method

--- test/RummyTest.cpp
+++ test/RummyTest.cpp
@@ -70,3 +70,112 @@ TEST(RummyTest, deal_cards_with_3_players) {
   delete michael;
   delete dwight;
 }
+
+// deal with 4 players
+TEST(RummyTest, deal_cards_with_4_players) {
+  MockDeck d;
+  MockUI ui;
+
+  EXPECT_CALL(d, getCard())
+  .Times(28);
+
+  Game* game = new Rummy(&ui, &d);
+  Player* michael = new Player("Michael");
+  Player* dwight = new Player("Dwight");
+  Player* jim = new Player("Jim");
+  Player* pam = new Player("Pam");
+
+  game->addPlayer(michael);
+  game->addPlayer(dwight);
+  game->addPlayer(jim);
+  game->addPlayer(pam);
+
+  EXPECT_EQ(game->getPlayers().size(), 4);
+
+  game->dealCards(game->getPlayers());
+
+  for (Player* p : game->getPlayers())
+    EXPECT_EQ(7, p->getHand()->size());
+
+  delete game;
+  delete jim;
+  delete pam;
+  delete michael;
+  delete dwight;
+}
+
+// deal with 5 players
+TEST(RummyTest, deal_cards_with_5_players) {
+  MockDeck d;
+  MockUI ui;
+
+  EXPECT_CALL(d, getCard())
+  .Times(30);
+
+  Game* game = new Rummy(&ui, &d);
+  Player* michael = new Player("Michael");
+  Player* dwight = new Player("Dwight");
+  Player* jim = new Player("Jim");
+  Player* pam = new Player("Pam");
+  Player* erin = new Player("Erin");
+
+
+  game->addPlayer(michael);
+  game->addPlayer(dwight);
+  game->addPlayer(jim);
+  game->addPlayer(pam);
+  game->addPlayer(erin);
+
+  EXPECT_EQ(game->getPlayers().size(), 5);
+
+  game->dealCards(game->getPlayers());
+
+  for (Player* p : game->getPlayers())
+    EXPECT_EQ(6, p->getHand()->size());
+
+  delete game;
+  delete erin;
+  delete jim;
+  delete pam;
+  delete michael;
+  delete dwight;
+}
+
+// deal with 6 players
+TEST(RummyTest, deal_cards_with_6_players) {
+  MockDeck d;
+  MockUI ui;
+
+  EXPECT_CALL(d, getCard())
+  .Times(36);
+
+  Game* game = new Rummy(&ui, &d);
+  Player* michael = new Player("Michael");
+  Player* dwight = new Player("Dwight");
+  Player* jim = new Player("Jim");
+  Player* pam = new Player("Pam");
+  Player* erin = new Player("Erin");
+  Player* ryan = new Player("Ryan");
+
+  game->addPlayer(michael);
+  game->addPlayer(dwight);
+  game->addPlayer(jim);
+  game->addPlayer(pam);
+  game->addPlayer(erin);
+  game->addPlayer(ryan);
+
+  EXPECT_EQ(game->getPlayers().size(), 6);
+
+  game->dealCards(game->getPlayers());
+
+  for (Player* p : game->getPlayers())
+    EXPECT_EQ(6, p->getHand()->size());
+
+  delete game;
+  delete ryan;
+  delete erin;
+  delete jim;
+  delete pam;
+  delete michael;
+  delete dwight;
+}
